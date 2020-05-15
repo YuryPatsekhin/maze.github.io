f101a07a73c5b537ae8fe7af00eb5ea7460ca20e
Yury Patsekhin <yury.patsekhin@pandadoc.com>
2020-05-16 02:41:55 +0300

    added possible to find the way

--- maze.js
+++ maze.js
@@ -4,16 +4,21 @@ let WALL;
 let CELL;
 let ENTRY;
 let EXIT;
+let ROAD;
 let height;
 let width;
 let listOfCells;
+let generatedMaze;
 let countOfUnvisitedCells;
+let coordinateOfEntry;
+let coordinateOfExit;
 
 const setInitialValues = () => {
     CELL = 0;
     WALL = 1;
     ENTRY = 2;
     EXIT = 3;
+    ROAD = 4;
     height = 21;
     width = 21;
     listOfCells = [];
@@ -47,28 +52,52 @@ const generateBasisOfMaze = (height, width) => {
     return generatedBasisOfMaze;
 };
 
-const getNeighbors = (cell) => {
+const getNeighbors = (cell, getNeighborsForFindTheWay) => {
     const neighbors = [];
-    if (cell.x - 2 > 0) {
-        const neighbor = findCellByCoordinates({ x: cell.x - 2, y: cell.y });
 
-        neighbors.push(neighbor)
-    };
-    if (cell.x + 2 < height - 1) {
-        const neighbor = findCellByCoordinates({ x: cell.x + 2, y: cell.y });
+    if (!getNeighborsForFindTheWay) {
+        if (cell.x - 2 > 0) {
+            const neighbor = findCellByCoordinates({ x: cell.x - 2, y: cell.y });
 
-        neighbors.push(neighbor);
-    };
-    if (cell.y + 2 < width - 1) {
-        const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y + 2 });
+            neighbors.push(neighbor)
+        };
+        if (cell.x + 2 < height - 1) {
+            const neighbor = findCellByCoordinates({ x: cell.x + 2, y: cell.y });
 
-        neighbors.push(neighbor);
-    };
-    if (cell.y - 2 > 0) {
-        const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y - 2 });
+            neighbors.push(neighbor);
+        };
+        if (cell.y + 2 < width - 1) {
+            const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y + 2 });
 
-        neighbors.push(neighbor);
+            neighbors.push(neighbor);
+        };
+        if (cell.y - 2 > 0) {
+            const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y - 2 });
+
+            neighbors.push(neighbor);
+        }
+    } else {
+        if (generatedMaze[cell.x - 1][cell.y] === CELL || generatedMaze[cell.x - 1][cell.y] === EXIT) {
+            const neighbor = findCellByCoordinates({ x: cell.x - 1, y: cell.y });
+            neighbors.push(neighbor);
+        };
+        if (generatedMaze[cell.x + 1][cell.y] === CELL || generatedMaze[cell.x + 1][cell.y] === EXIT) {
+            const neighbor = findCellByCoordinates({ x: cell.x + 1, y: cell.y });
+
+            neighbors.push(neighbor);
+        }
+        if (generatedMaze[cell.x][cell.y + 1] === CELL || generatedMaze[cell.x][cell.y + 1] === EXIT) {
+            const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y + 1 });
+
+            neighbors.push(neighbor);
+        }
+        if (generatedMaze[cell.x][cell.y - 1] === CELL || generatedMaze[cell.x][cell.y - 1] === EXIT) {
+            const neighbor = findCellByCoordinates({ x: cell.x, y: cell.y - 1 });
+
+            neighbors.push(neighbor);
+        }
     }
+
 
     return neighbors;
 }
@@ -92,8 +121,9 @@ const breakTheWall = (firstCell, secondCell, basis) => {
     }
 
     const height = coordinateOfWall.x;
-    const positionOnRow = coordinateOfWall.y
-    listOfCells.push({ x: coordinateOfWall.x, y: coordinateOfWall.y, visited: true })
+    const positionOnRow = coordinateOfWall.y;
+
+    listOfCells.push({ x: coordinateOfWall.x, y: coordinateOfWall.y, visited: true });
 
     basis[height].splice(positionOnRow, 1, 0);
 
@@ -108,10 +138,9 @@ const findCellByCoordinates = (coordinates) => {
 
 const generateMaze = (height, width) => {
     let basis = generateBasisOfMaze(height, width);
-    const startCellCoordinate = { x: 1, y: 1 };
-    let currentCell = findCellByCoordinates(startCellCoordinate);
+    let currentCell = { x: 1, y: 1 };
 
-    findCellByCoordinates(startCellCoordinate).visited = true;
+    findCellByCoordinates(currentCell).visited = true;
     countOfUnvisitedCells--;
 
     while (countOfUnvisitedCells > 0) {
@@ -144,6 +173,7 @@ const getRandomCell = () => {
 const generateEntry = (maze) => {
     const randomCell = getRandomCell();
 
+    coordinateOfEntry = { x: randomCell.x, y: randomCell.y };
     maze[randomCell.x].splice(randomCell.y, 1, ENTRY);
     return;
 }
@@ -151,6 +181,7 @@ const generateEntry = (maze) => {
 const generateExit = (maze) => {
     const randomCell = getRandomCell();
 
+    coordinateOfExit = { x: randomCell.x, y: randomCell.y };
     maze[randomCell.x].splice(randomCell.y, 1, EXIT);
     return;
 }
@@ -170,15 +201,29 @@ const drawMaze = (generatedMaze) => {
                 wall.classList.add('wall');
                 row.appendChild(wall);
             } else if (cell === ENTRY) {
+                const space = document.createElement('div');
                 const entry = document.createElement('div');
 
+                space.classList.add('space');
                 entry.classList.add('entry');
-                row.appendChild(entry);
+                space.appendChild(entry);
+                row.appendChild(space);
             } else if (cell === EXIT) {
+                const space = document.createElement('div');
                 const exit = document.createElement('div');
 
+                space.classList.add('space');
                 exit.classList.add('exit');
-                row.appendChild(exit);
+                space.appendChild(exit);
+                row.appendChild(space);
+            } else if (cell === ROAD) {
+                const space = document.createElement('div');
+                const road = document.createElement('div');
+
+                space.classList.add('space');
+                road.classList.add('road');
+                space.appendChild(road);
+                row.appendChild(space);
             } else {
                 const space = document.createElement('div');
 
@@ -190,15 +235,56 @@ const drawMaze = (generatedMaze) => {
     });
 };
 
-const createMaze = () => {
-    let generatedMaze;
+const drawTheWay = (wayToExit) => {
+    let entry = findCellByCoordinates(coordinateOfEntry);
+    let exit = findCellByCoordinates(coordinateOfExit);
 
+    wayToExit.forEach(el => {
+        if (el !== entry && el !== exit) {
+            generatedMaze[el.x].splice(el.y, 1, ROAD);
+        }
+    });
+
+    drawMaze(generatedMaze);
+}
+
+const findTheWay = () => {
+    document.querySelector('.start').removeEventListener('click', findTheWay);
+    const getNeighborsForFindTheWay = true;
+    const wayToExit = [];
+    let entry = findCellByCoordinates(coordinateOfEntry);
+    let exit = findCellByCoordinates(coordinateOfExit);
+
+    wayToExit.push(entry);
+    listOfCells.forEach(el => el.visited = false);
+    entry.visited = true;
+
+    while (wayToExit[wayToExit.length - 1] !== exit) {
+        const neighbors = getNeighbors(wayToExit[wayToExit.length - 1], getNeighborsForFindTheWay);
+        const unvisitedNeigebors = neighbors.filter(neighbor => !neighbor.visited);
+
+        if (unvisitedNeigebors.length > 0) {
+            const nextCell = getRandomNeighbor(unvisitedNeigebors);
+
+            wayToExit.push(nextCell);
+            nextCell.visited = true;
+        } else {
+            wayToExit.pop();
+        }
+    }
+
+    drawTheWay(wayToExit);
+}
+
+const createMaze = () => {
     setInitialValues();
     generatedMaze = generateMaze(height, width);
     generateEntry(generatedMaze);
     generateExit(generatedMaze);
     drawMaze(generatedMaze);
+    document.querySelector('.start').addEventListener('click', findTheWay);
 }
 
 createMaze();
+
 document.querySelector('.generate').addEventListener('click', createMaze);
--- scripts/character.js
+++
@@ -1,3 +0,0 @@
-export default {
-    character: () => {console.log(1)}
-};
